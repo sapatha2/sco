fff7c3dcc89cbc591e2ad38980b1d636d39c4141
Shivesh Pathak <sapatha2@illinois.edu>
2017-12-01 10:53:33 -0600

    Preliminary spin and charge densities done for HSE, PBE0. Need to look closer

---
+++ undoped/PBE0/AFM_small/calc_dens.py
@@ -0,0 +1,13 @@
+import cubetools
+nup=66
+ndown=66
+up=cubetools.read_cube("qwalk_000.plot_u.dens.cube")
+down=cubetools.read_cube("qwalk_000.plot_d.dens.cube")
+up=cubetools.normalize_abs(up)
+down=cubetools.normalize_abs(down)
+up['data']*=nup
+down['data']*=ndown
+up['data']-=down['data'] #make 'up' the spin density
+cubetools.write_cube(up,"qwalk_000.spin.dens.cube")
+up['data']+=2*down['data'] #make 'up' the charge density
+cubetools.write_cube(up,"qwalk_000.chg.dens.cube")
