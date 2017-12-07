251f99e76569043e443c900c0c5e5c55cefaa07e
Shivesh Pathak <sapatha2@illinois.edu>
2017-12-07 16:18:33 -0600

    Finished constructing excited states. Now time to do QMC

--- undoped/HSE/FM_small/calc_dens.py
+++ undoped/HSE/FM_small/calc_dens.py
@@ -1,13 +1,25 @@
 import cubetools
 nup=68
 ndown=64
+
 up=cubetools.read_cube("qwalk_000.plot_u.dens.cube")
 down=cubetools.read_cube("qwalk_000.plot_d.dens.cube")
 up=cubetools.normalize_abs(up)
 down=cubetools.normalize_abs(down)
 up['data']*=nup
-down['data']*=ndown
-up['data']-=down['data'] #make 'up' the spin density
-cubetools.write_cube(up,"qwalk_000.spin.dens.cube")
-up['data']+=2*down['data'] #make 'up' the charge density
-cubetools.write_cube(up,"qwalk_000.chg.dens.cube")
+#down['data']*=ndown
+#up['data']-=down['data'] #make 'up' the spin density
+#cubetools.write_cube(up,"qwalk_000.spin.dens.cube")
+#up['data']+=2*down['data'] #make 'up' the charge density
+#cubetools.write_cube(up,"qwalk_000.chg.dens.cube")
+
+
+#CT state
+down_ct=cubetools.read_cube("qwalk_000.plot_d_ct1.dens.cube")
+down_ct=cubetools.normalize_abs(down_ct)
+down_ct['data']*=ndown
+up['data']-=down_ct['data'] #make 'up' the spin density
+cubetools.write_cube(up,"qwalk_000.ct1_spin.dens.cube")
+up['data']+=2*down_ct['data'] #make 'up' the charge density
+cubetools.write_cube(up,"qwalk_000.ct1_chg.dens.cube")
+up['data']-=down_ct['data']
--- undoped/PBE0/FM_small/calc_dens.py
+++ undoped/PBE0/FM_small/calc_dens.py
@@ -11,3 +11,25 @@ up['data']-=down['data'] #make 'up' the spin density
 cubetools.write_cube(up,"qwalk_000.spin.dens.cube")
 up['data']+=2*down['data'] #make 'up' the charge density
 cubetools.write_cube(up,"qwalk_000.chg.dens.cube")
+up['data']-=down['data']
+
+#CT state
+down_ct=cubetools.read_cube("qwalk_000.plot_d_ct1.dens.cube")
+down_ct=cubetools.normalize_abs(down_ct)
+down_ct['data']*=ndown
+up['data']-=down_ct['data'] #make 'up' the spin density
+cubetools.write_cube(up,"qwalk_000.ct1_spin.dens.cube")
+up['data']+=2*down_ct['data'] #make 'up' the charge density
+cubetools.write_cube(up,"qwalk_000.ct1_chg.dens.cube")
+up['data']-=down_ct['data']
+
+down_ct=cubetools.read_cube("qwalk_000.plot_d_ct2.dens.cube")
+down_ct=cubetools.normalize_abs(down_ct)
+down_ct2=cubetools.read_cube("qwalk_000.plot_d_ct3.dens.cube")
+down_ct2=cubetools.normalize_abs(down_ct2)
+down_ct['data']*=ndown
+down_ct2['data']*=ndown
+up['data']-=(down_ct['data']+down_ct2['data'])/2 #make 'up' the spin density
+cubetools.write_cube(up,"qwalk_000.ct2_spin.dens.cube")
+up['data']+=(down_ct['data']+down_ct['data']) #make 'up' the charge density
+cubetools.write_cube(up,"qwalk_000.ct2_chg.dens.cube")
