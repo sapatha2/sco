104b0662f586ee8ac35b9885087677b5fd5d7f5c
Shivesh Pathak <sapatha2@illinois.edu>
2018-01-23 14:16:01 -0600

    Finished all plots for PBE0

--- doped_fv/PBE0/COL_ns/plots/diff_dens.py
+++ doped_fv/PBE0/COL_ns/plots/diff_dens.py
@@ -1,14 +1,25 @@
 import cubetools
 import copy 
 
+'''
 s1=cubetools.read_cube("qwalk_000.spin.dens.cube")
-s2=cubetools.read_cube("../../../../undoped/PBE0/COL/plots/qwalk_000.spin.dens.cube")
+s2=cubetools.read_cube("../../../../undoped/pbe0/col/plots/qwalk_000.spin.dens.cube")
 
 c1=cubetools.read_cube("qwalk_000.chg.dens.cube")
-c2=cubetools.read_cube("../../../../undoped/PBE0/COL/plots/qwalk_000.chg.dens.cube")
+c2=cubetools.read_cube("../../../../undoped/pbe0/col/plots/qwalk_000.chg.dens.cube")
 
 s1['data']-=s2['data'] #make 'up' the spin density
 cubetools.write_cube(s1,"qwalk_000.spin_diff.dens.cube")
 c1['data']-=c2['data'] #make 'up' the charge density
 cubetools.write_cube(c1,"qwalk_000.chg_diff.dens.cube")
+'''
+s1=cubetools.read_cube("qwalk_000.spin.dens.cube")
+s2=cubetools.read_cube("../../CHK_ns/plots/qwalk_000.spin.dens.cube")
 
+c1=cubetools.read_cube("qwalk_000.chg.dens.cube")
+c2=cubetools.read_cube("../../CHK_ns/plots/qwalk_000.chg.dens.cube")
+
+s1['data']-=s2['data'] #make 'up' the spin density
+cubetools.write_cube(s1,"qwalk_000.spin_diff2.dens.cube")
+c1['data']-=c2['data'] #make 'up' the charge density
+cubetools.write_cube(c1,"qwalk_000.chg_diff2.dens.cube")
