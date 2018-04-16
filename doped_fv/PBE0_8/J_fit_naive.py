dfeec17897ef6e47f6026e933d778cf211c9c06e
Shivesh Pathak <sapatha2@illinois.edu>
2018-04-16 13:04:28 -0500

    Small edit

--- doped_fv/PBE0_8/J_fit_naive.py
+++ doped_fv/PBE0_8/J_fit_naive.py
@@ -4,6 +4,8 @@ from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score
 
+'''
+#All
 sigJ=np.array([-4,-4,0,-2,-2,-2,0,0,0,0,0,0,4,4])
 name=np.array(['CHK0','CHK2','COL0','FLP0','FLP2','FLP4','BCOL0','BCOL2','BLK0','ACHN2','ACHN4','ACHN6','FM8','FM10'])
 E_PBE0=np.array([-1.8418450389802E+03,-1.8418562836080E+03,-1.8418601953776E+03,
@@ -18,24 +20,15 @@ name_sort=name[ind]
 plt.plot((E_sort-E_sort[0])*27.2,'o')
 plt.xticks(np.arange(0,14),name_sort)
 plt.show()
-
 '''
-E0=E_PBE0[0]
-E_PBE0-=E0
-E_PBE0*=27.2
+name=np.array(['CHK0','COL0','FLP2','BCOL0','BLK0','ACHN4','FM8'])
+E_PBE0=np.array([-1.8418450389802E+03,-1.8418601953776E+03,
+-1.8418566128030E+03,-1.8418284707358E+03,-1.8418522579094E+03,
+-1.8418248027241E+03,-1.8417482260516E+03])
 
-fit_PBE0 = linear_model.LinearRegression(fit_intercept=True)
-fit_PBE0.fit(sigJ[:,np.newaxis],E_PBE0)
-
-E_PBE0_pred=fit_PBE0.predict(sigJ[:,np.newaxis])
-
-print(fit_PBE0.coef_[0],r2_score(E_PBE0,E_PBE0_pred))
-plt.plot(E_PBE0,E_PBE0_pred,'bo')
-plt.plot(E_PBE0,E_PBE0,'g')
-
-plt.ylabel("E(pred), eV)")
-plt.xlabel("E-E(CHK), eV")
-
-plt.title("J="+str(fit_PBE0.coef_[0])+", R2="+str(r2_score(E_PBE0,E_PBE0_pred)))
+ind=np.argsort(E_PBE0)
+E_sort=E_PBE0[ind]
+name_sort=name[ind]
+plt.plot((E_sort-E_sort[0])*27.2,'o')
+plt.xticks(np.arange(0,14),name_sort)
 plt.show()
-'''
