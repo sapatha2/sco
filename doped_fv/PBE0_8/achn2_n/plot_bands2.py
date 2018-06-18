9ded6a567061472aa53047f7e9d67a6114c9d32b
Shivesh Pathak <sapatha2@illinois.edu>
2018-06-18 15:25:47 -0500

    achn2_ bands fixed

--- doped_fv/PBE0_8/achn2_n/plot_bands2.py
+++ doped_fv/PBE0_8/achn2_n/plot_bands2.py
@@ -43,7 +43,7 @@ for b in range(130,135):
   ax.plot(range(1,nkpt+1),conv*bands[0][:,b],color='k')
   ax.plot(range(1,nkpt+1),conv*bands[1][:,b],color='r')
 
-P.ylim([-4,4])
+P.ylim([-10,2])
 P.ylabel("Energy (eV) ")
 if len(sys.argv) < 2: 
   P.show()
