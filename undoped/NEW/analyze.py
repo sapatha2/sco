25ba69bec65d5f4ee89eeff7882a5cd6e607e01b
Shivesh Pathak <sapatha2@illinois.edu>
2019-02-11 17:30:02 -0600

    did a fit with 1-band hubbard model, not bad at all!

--- undoped/NEW/analyze.py
+++ undoped/NEW/analyze.py
@@ -7,32 +7,47 @@ import statsmodels.api as sm
 
 def analyze(df):
   print(df)
-  #Pairplot
-  sns.pairplot(df,vars=['energy','sigT','sigU','sigJ'],hue='path',markers=['s']*3+['o']*3)
-  plt.savefig('plots/vmc_pairplot.pdf',bbox_inches='tight')
-  plt.close()
 
-  #Fit
+  '''
+  #Residuals
   y=df['energy']
-  X=df[['sigT','sigU']]
+  X=df[['sigU']]
   X=sm.add_constant(X)
   ols=sm.OLS(y,X).fit()
   print(ols.summary())
+  df['resid']=df['energy']-ols.predict(X)
+  sns.pairplot(df,vars=['resid','sigT','sigU','sigJ'],hue='path',markers=['s']*3+['o']*3)
+  plt.show()
+  '''
+
+  #Pairplot
+  #df['sigNd+sigNps']=df['sigNd']+df['sigNps']
+  #sns.pairplot(df,vars=['energy','sigTd','sigU'],hue='path',markers=['s']*3+['o']*3)
+  #plt.savefig('plots/vmc_pairplot.pdf',bbox_inches='tight')
+  #plt.close()
+  #plt.show()
+
+  #Fit
+  y=df['energy']
+  X=df[['sigTd','sigU']]
+  X=sm.add_constant(X)
+  ols=sm.OLS(y,X).fit()
+  print(ols.summary())
+  '''
   plt.errorbar(ols.predict(X),df['energy'],yerr=df['energy_err'],fmt='bo')
   plt.plot(df['energy'],df['energy'],'g-')
   plt.ylabel('energy (eV)')
   plt.xlabel('pred (eV)')
-  plt.savefig('plots/vmc_pred.pdf',bbox_inches='tight')
- 
+  plt.savefig('plots/vmc_pred_Td.pdf',bbox_inches='tight')
   '''
-  import matplotlib.pyplot as plt
+
   from mpl_toolkits.mplot3d import Axes3D
   fig = plt.figure()
   ax = fig.add_subplot(111, projection='3d')
   #Values with errorbars
   f=df.values
-  fx=f[:,2] #sigT
-  fy=f[:,3] #sigU
+  fx=f[:,3] #sigTd
+  fy=f[:,4] #sigU
   fz=f[:,0] #energy
   zerror=f[:,1] #energy_err
   ax.plot(fx, fy, fz, "bo")
@@ -46,11 +61,10 @@ def analyze(df):
   zv=ols.params[0] + ols.params[1]*xv + ols.params[2]*yv 
   ax.plot_surface(xv,yv,zv,cmap=plt.cm.coolwarm)
 
-  ax.set_xlabel('sigT')
+  ax.set_xlabel('sigTd')
   ax.set_ylabel('sigU')
   ax.set_zlabel('E (eV)')
   plt.show()
-  '''
 
 if __name__=='__main__':
   df=np.load('p_gosling.pickle')
---
+++ undoped/NEW/scf/analyze.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+df=pd.read_pickle('scf_gosling.pickle')
+#df['N']=df['sigNpp']+df['sigNd']+df['sigNps']#+df['sigN4s']+df['sigN2s'] #Last two terms get the leftover 25% of electron under excitation
+#print(np.sqrt(df['N'].var()))
+sns.pairplot(df,vars=['sigNps','sigNd','sigT','sigU'],hue='state')
+plt.show()
